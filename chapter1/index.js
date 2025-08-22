86c7c8ae678bc13461669d3799e254481ad833f3
Suraj Nair <surajnair9698@gmail.com>
2025-08-22 17:43:50 +0530

    Creating an object using object literal

--- chapter1/index.js
+++ chapter1/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const circle = {
+  radius: 1,
+  //object property
+  location: {
+    //object property
+    x: 1,
+    y: 1,
+  },
+  draw: function () {
+    //object method
+    console.log("draw");
+  },
+};
